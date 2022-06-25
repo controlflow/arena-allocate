1424489f5766d03307a5ea0ec273917b9a611a38
Alexander.Shvedov <alexander.shvedov@jetbrains.com>
2022-06-25 22:18:50 +0300

    inital

---
+++ src/ArenaAlloc/ArenaAlloc/Program.cs
@@ -0,0 +1,15 @@
+﻿#nullable enable
+namespace ArenaAlloc;
+
+public sealed class Arena<T>
+  where T : class, IArenaParticipant
+{
+  private readonly Func<T> myFactory;
+  private readonly T[] myArray;
+  private int myFreeIndex;
+}
+
+public interface IArenaParticipant
+{
+  void ClearAllReferences();
+}
